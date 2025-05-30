34e22bf0bf1ffea776e6ccb3916d27ff08230f72
aalesh01 <aalesh111999@gmail.com>
2025-05-30 08:40:31 +0530

    updated base url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://ticketing-tool-mnma.onrender.com:3001/api';
+export const API_BASE_URL = 'https://ticketing-tool-mnma.onrender.com/api';
 
 export const API_ENDPOINTS = {
   AUTH: {
